9eafd457529e6badceeb07feec1ace9c1c6475e4
RuitaoLiang <35542361+RuitaoLiang@users.noreply.github.com>
2018-01-19 12:59:06 -0800

    Create puzzle7.js

---
+++ puzzle7.js
@@ -0,0 +1,18 @@
+function upandDown {
+ While(getColor!="red") {
+  down
+ }
+ right
+ right
+ while(getColor!="red") {
+  up
+ }
+ right
+ right
+}
+upandDown
+upandDown
+While(getColor!="red") {
+  down
+}
+right
