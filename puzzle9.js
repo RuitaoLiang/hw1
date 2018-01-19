f065199aa2ffacce1dbc19021317d481043e2c2c
RuitaoLiang <35542361+RuitaoLiang@users.noreply.github.com>
2018-01-19 13:23:32 -0800

    Create puzzle9.js

---
+++ puzzle9.js
@@ -0,0 +1,12 @@
+function drawing(direction,n) {
+var count=0
+while(count<7) {
+  setColor("blue")
+  direction
+  count=count+1
+ }
+}
+drawing(right,7)
+drawing(down,5)
+drawing(left,7)
+drawing(up,5)
