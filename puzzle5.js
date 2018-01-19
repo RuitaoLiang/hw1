9638cfb62cf2f9cba3300c83043f2e3ff5154780
RuitaoLiang <35542361+RuitaoLiang@users.noreply.github.com>
2018-01-19 11:36:09 -0800

    Create puzzle5.js

---
+++ puzzle5.js
@@ -0,0 +1,22 @@
+right
+right
+function goOneStep {
+if(getColor == "blue") {
+    down
+  }
+if(getColor == "red") {
+  up
+  }
+}
+right
+right
+goOneStep
+right
+right
+goOneStep
+right
+right
+right
+
+
+ 
