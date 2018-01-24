180c8263e023c61099b3107719e87d472ccbaac0
RuitaoLiang <35542361+RuitaoLiang@users.noreply.github.com>
2018-01-24 08:42:11 -0800

    Update puzzle2.js

--- puzzle2.js
+++ puzzle2.js
@@ -1,15 +1,15 @@
-function downThreeTimes {
-  down
-  down
-  down
+function downThreeTimes(){
+  down();
+  down();
+  down();
 }
-downThreeTimes
-downThreeTimes
-right
-right
-right
-up
-up
-right
-right
-downThreeTimes
+downThreeTimes();
+downThreeTimes();
+right();
+right();
+right();
+up();
+up();
+right();
+right();
+downThreeTimes();
