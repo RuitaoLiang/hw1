240b5b7129bf367c1a3d0e876d224a6e0d6da63a
RuitaoLiang <35542361+RuitaoLiang@users.noreply.github.com>
2018-01-24 09:25:19 -0800

    Create Challenges1.js

---
+++ Challenges1.js
@@ -0,0 +1,28 @@
+function drawing(direction,n) {
+var count=0
+while(count<n) {
+  setColor("blue")
+  direction();
+  count=count+1
+ }
+}
+drawing(right,7)
+drawing(down,5)
+drawing(left,7)
+drawing(up,5)
+right();
+right();
+right();
+right();
+right();
+right();
+right();
+right();
+right();
+right();
+right();
+right();
+drawing(right,6)
+drawing(down,4)
+drawing(left,6)
+drawing(up,4)
